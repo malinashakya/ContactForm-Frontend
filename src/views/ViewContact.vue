4e56c62b586bed2246150d20f9964f274e1037d1
Malina Shakya <126803519+malinashakya@users.noreply.github.com>
2024-09-13 16:27:04 +0545

    Added yup validation and real time validation in ViewContact

--- src/views/ViewContact.vue
+++ src/views/ViewContact.vue
@@ -7,20 +7,25 @@
     <div v-if="error">{{ error }}</div>
     <!-- Display table only if data is loaded and no errors occurred -->
     <div class="p-d-flex p-flex-column p-ai-center p-4">
-      <DataTable v-if="!loading && !error" :value="contacts" show-gridlines tableStyle="min-width: 50rem" >
-        <Column class="p-2" field="id" header="ID"  ></Column>
-        <Column class="p-2" field="name" header="Name" ></Column>
-        <Column class="p-2" field="address" header="Address" ></Column>
-        <Column class="p-2" field="contact" header="Contact" ></Column>
-        <Column class="p-2" field="email" header="Email" ></Column>
-        <Column class="p-2 " field="message" header="Message" ></Column>
+      <DataTable v-if="!loading && !error" :value="contacts" show-gridlines tableStyle="min-width: 50rem">
+        <Column class="p-2" field="id" header="ID"></Column>
+        <Column class="p-2" field="name" header="Name"></Column>
+        <Column class="p-2" field="address" header="Address"></Column>
+        <Column class="p-2" field="contact" header="Contact"></Column>
+        <Column class="p-2" field="email" header="Email"></Column>
+        <Column class="p-2 " field="message" header="Message"></Column>
         <Column class="p-2" header="Action">
           <template #body="slotProps">
             <!-- Button to open the edit dialog for a contact -->
-            <Button class="m-2 p-2" style="background: #eded07; border: 1px solid rgba(244,244,73,0.89); border-radius: 3px" @click="openEditDialog(slotProps.data)"> Edit</Button>
+            <Button class="m-2 p-2"
+                    style="background: #eded07; border: 1px solid rgba(244,244,73,0.89); border-radius: 3px"
+                    @click="openEditDialog(slotProps.data)"> Edit
+            </Button>
 
             <!-- Button to delete a contact -->
-            <Button class="m-2 p-2" style="background: red; border: 1px solid #e35555; border-radius: 3px" @click="deleteContact(slotProps.data.id)">Delete</Button>
+            <Button class="m-2 p-2" style="background: red; border: 1px solid #e35555; border-radius: 3px"
+                    @click="deleteContact(slotProps.data.id)">Delete
+            </Button>
           </template>
         </Column>
       </DataTable>
@@ -35,41 +40,48 @@
       <form class="p-fluid" @submit.prevent="updateContact">
         <!-- Input for the contact's name -->
         <div class="field">
-          <label for="name">Name:</label>
+          <label for="name">Name:<span class="required">*</span></label>
           <InputText id="name" v-model="editedContact.name" class="p-3 custom-input " required type="text"/>
+          <div v-if="formErrors.name" class="error">{{ formErrors.name }}</div>
         </div>
 
         <!-- Input for the contact's address -->
         <div class="field">
-          <label for="address">Address:</label>
+          <label for="address">Address:<span class="required">*</span></label>
           <InputText id="address" v-model="editedContact.address" class="p-3 custom-input" required type="text"/>
+          <div v-if="formErrors.address" class="error">{{ formErrors.address }}</div>
         </div>
 
         <!-- Input for the contact's phone number -->
         <div class="field">
-          <label for="contact">Contact:</label>
+          <label for="contact">Contact:<span class="required">*</span></label>
           <InputText id="contact" v-model="editedContact.contact" class="p-3 custom-input" required type="text"/>
+          <div v-if="formErrors.contact" class="error">{{ formErrors.contact }}</div>
         </div>
 
         <!-- Input for the contact's email -->
         <div class="field">
-          <label for="email">Email:</label>
+          <label for="email">Email:<span class="required">*</span></label>
           <InputText id="email" v-model="editedContact.email" class="p-3 custom-input" required type="email"/>
+          <div v-if="formErrors.email" class="error">{{ formErrors.email }}</div>
         </div>
 
         <!-- Textarea for the contact's message -->
         <div class="field">
-          <label for="message">Message:</label>
+          <label for="message">Message:<span class="required">*</span></label>
           <InputText id="message" v-model="editedContact.message" class="p-2 border rounded custom-textarea"
                      required></InputText>
+          <div v-if="formErrors.message" class="error">{{ formErrors.message }}</div>
         </div>
 
 
-        <Button class="button p-2 m-2" style="background: #eded07; border: 1px solid rgba(244,244,73,0.89); border-radius: 3px " type="submit">
+        <Button class="button p-2 m-2"
+                style="background: #eded07; border: 1px solid rgba(244,244,73,0.89); border-radius: 3px " type="submit">
           Submit
         </Button>
         <!-- Button to close the dialog without making changes -->
-        <Button class="button p-2 m-2 " style="background: red; border: 1px solid #e35555; border-radius: 3px" @click="closeEditDialog">
+        <Button class="button p-2 m-2 " style="background: red; border: 1px solid #e35555; border-radius: 3px"
+                @click="closeEditDialog">
           Cancel
         </Button>
 
@@ -82,12 +94,13 @@
 </template>
 
 <script lang="ts" setup>
-import {ref, onMounted} from 'vue';
+import {ref, onMounted, watch} from 'vue';
 import axios from 'axios';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
 import Dialog from 'primevue/dialog';
 import InputText from 'primevue/inputtext';
+import * as yup from 'yup';
 
 
 // Define types for the contact data
@@ -107,6 +120,64 @@ const error = ref<string | null>(null);
 const showEditDialog = ref(false);
 const editedContact = ref<Contact | null>(null);
 const updateSuccess = ref<string | null>(null);
+const formErrors=ref({
+  name:'',
+  address:'',
+  contact:'',
+  email:'',
+  message:'',
+});
+
+const schema=yup.object().shape({
+  name:yup.string().min('2').required('Name is required')
+      .matches(/^[A-Za-z\s]+$/, 'Name can only contain letters and spaces'),
+  address: yup.string().required('Address is required'),
+  contact:yup.string().required('Contact is required'),
+  email:yup.string().required('Email is required').email('Invalid email'),
+  message:yup.string().required('Message is required'),
+})
+
+const validateField = async (field: keyof Contact) => {
+  try {
+    if (editedContact.value) {
+      await schema.validateAt(field, editedContact.value);
+      formErrors.value[field] = '';
+    }
+  } catch (err) {
+    if (err instanceof yup.ValidationError) {
+      formErrors.value[field] = err.message;
+    }
+  }
+};
+
+const validateForm = async () => {
+  try {
+    // Resetting form errors before validation
+    Object.keys(formErrors.value).forEach(key => (formErrors.value[key] = ''));
+
+    // Validate form data against Yup schema
+    if (editedContact.value) {
+      await schema.validate(editedContact.value, { abortEarly: false });
+      return true;
+    }
+    return false;
+  } catch (err) {
+    // Set form errors for each field
+    if (err instanceof yup.ValidationError) {
+      err.inner.forEach(validationError => {
+        formErrors.value[validationError.path] = validationError.message;
+      });
+    }
+    return false;
+  }
+};
+
+// Watchers for individual fields
+watch(() => editedContact.value?.name, () => validateField('name'));
+watch(() => editedContact.value?.email, () => validateField('email'));
+watch(() => editedContact.value?.address, () => validateField('address'));
+watch(() => editedContact.value?.message, () => validateField('message'));
+watch(() => editedContact.value?.contact, () => validateField('contact'));
 
 // Fetch contact data when the component is mounted
 onMounted(async () => {
@@ -172,10 +243,11 @@ const deleteContact = async (id: number) => {
 <style scoped>
 .custom-input,
 .custom-textarea {
-  background-color: lightgray;
-  border: 1px solid lightgray;
+  background-color: white;
+  color:black;
+  border: 1px solid white;
   border-radius: 5px;
-  margin: 5px 20px;
+  margin-left:10px;
 }
 
 .custom-textarea {
@@ -189,5 +261,16 @@ const deleteContact = async (id: number) => {
   width: 90px;
   height: 40px;
   color: black;
+}
+
+.error {
+  color: rgba(255, 0, 0, 0.85);
+  font-size: 1rem;
+  margin-top: 0.5rem;
+}
+
+.required {
+  padding-left: 2px;
+  color: rgba(255, 0, 0, 0.85);
 }
 </style>
