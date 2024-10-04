ecc684e4aa41eee71d7fe43a9e2d01eeeb0e54e7
Malina Shakya <126803519+malinashakya@users.noreply.github.com>
2024-10-04 15:18:51 +0545

    Proper validation for each field done

--- src/views/DynamicAlignmentFormVeeValidation.vue
+++ src/views/DynamicAlignmentFormVeeValidation.vue
@@ -1,7 +1,8 @@
 <script setup>
 import { configure, defineRule, ErrorMessage, Field, Form } from "vee-validate";
 import Button from "primevue/button";
-import { required } from '@vee-validate/rules';
+import {integer, min, regex, required} from '@vee-validate/rules';
+import {computed} from "vue";
 
 const handleSubmit = () => {
   console.log("Form submitted");
@@ -9,7 +10,34 @@ const handleSubmit = () => {
 
 // Define rules
 defineRule('required', required);
+defineRule('min',min);
+defineRule('integer',integer);
+defineRule('regex',regex);
 
+defineRule('phone',(value)=>{
+  const phonePattern=/^\d{10}$/;
+  if(!phonePattern.test(value)){
+    return "Contact must be 10 digit number.";
+  }
+  return true;
+});
+
+defineRule('lettersOnly',(value)=>{
+  const strings=/^[A-Za-z]+$/;
+  if(!strings.test(value)){
+    return "Must be letters only";
+  }
+  return true;
+    }
+)
+
+defineRule('age',(value)=>{
+  if(value<1 || value>120)
+  {
+    return "Age must be between 1 and 120";
+  }
+  return true;
+})
 // Customized messages
 configure({
   validateOnInput: true,
@@ -17,23 +45,26 @@ configure({
     const fieldName = context.field;
     const message = {
       required: `${fieldName} is required.`,
+      min: `${fieldName.charAt(0).toUpperCase() + fieldName.slice(1)} should be at least ${context.rule?.params[0]} characters.`,
+            integer:`${fieldName.charAt(0).toUpperCase()+fieldName.slice(1)} should be numbers only`
     };
     return message[context.rule?.name] || `Invalid ${fieldName}.`;
   },
 });
+
 </script>
 
 <template>
-  <Form class="form-grid " @submit="handleSubmit">
+  <Form class="form-grid" @submit="handleSubmit">
 
     <!-- Name Field -->
     <div class="form-row d-contents">
-      <label for="name" class="label">Nameeeeeffffffeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeefffffffffffffffffffffffffeeeeeeeeeeeeeeeeeeeeeeeee</label>
+      <label for="name" class="label">Nammmmmmmmmmmmmmmmeeeeeeeeeee</label>
       <div class="form-field">
-        <Field v-slot="{ field }" name="name" rules="required">
+        <Field v-slot="{ field }" name="name" rules="required|min:2|lettersOnly">
           <InputText id="name" class="w-4.5" placeholder="Name" v-bind="field"/>
         </Field>
-        <ErrorMessage class="p-error ml-3 text-primary" name="name"/>
+        <ErrorMessage class="p-error ml-3" name="name"/>
       </div>
     </div>
 
@@ -41,7 +72,7 @@ configure({
     <div class="form-row">
       <label for="address" class="label">Address</label>
       <div class="form-field">
-        <Field v-slot="{ field }" name="address" rules="required">
+        <Field v-slot="{ field }" name="address" rules="required|min:2|lettersOnly">
           <InputText id="address" class="w-4.5" placeholder="Address" v-bind="field"/>
         </Field>
         <ErrorMessage class="p-error ml-3" name="address"/>
@@ -52,7 +83,7 @@ configure({
     <div class="form-row">
       <label for="contact" class="label">Contact</label>
       <div class="form-field">
-        <Field v-slot="{ field }" name="contact" rules="required">
+        <Field v-slot="{ field }" name="contact" rules="required|phone">
           <InputText id="contact" class="w-4.5" placeholder="Contact" v-bind="field"/>
         </Field>
         <ErrorMessage class="p-error ml-3" name="contact"/>
@@ -63,7 +94,7 @@ configure({
     <div class="form-row">
       <label for="age" class="label">Age</label>
       <div class="form-field">
-        <Field v-slot="{ field }" name="age" rules="required">
+        <Field v-slot="{ field }" name="age" rules="required|integer|age">
           <InputText id="age"  class="w-4.5" placeholder="Age" v-bind="field"/>
         </Field>
         <ErrorMessage class="p-error ml-3" name="age"/>
@@ -81,7 +112,7 @@ configure({
 </template>
 
 <style scoped>
-/* Form grid layout */
+
 .form-grid {
   display: grid;
   grid-template-columns: auto 1fr; /* Labels take natural width, inputs take remaining space */
